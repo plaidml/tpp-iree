46dc22ca5bfd2dc51db151da39a73cc8179336e2
Renato Golin <rengolin@systemcall.eu>
2023-01-17 14:46:23 +0000

    friendly git llvm check

--- scripts/git-llvm-check.py
+++ scripts/git-llvm-check.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 
 import git
+from datetime import datetime
 
 iree_llvm = git.Repo("./repos/iree/third_party/llvm-project")
 iree_commit = iree_llvm.head.commit
+iree_date = iree_commit.committed_date
+print("IREE's LLVM branch is at " + datetime.fromtimestamp(iree_date).isoformat())
+
 tpp_llvm = git.Repo("./repos/llvm-project")
 tpp_commit = tpp_llvm.head.commit
-print(iree_commit)
-print(tpp_commit)
-iree_date = iree_commit.committed_date
 tpp_date = tpp_commit.committed_date
-print(iree_date)
-print(tpp_date)
+print("TPP-MLIR's LLVM branch is at " + datetime.fromtimestamp(tpp_date).isoformat())
+
 if tpp_date > iree_date:
     print("Can't build this TPP version, LLVM too new for IREE")
 elif iree_date > tpp_date:
